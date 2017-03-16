3e5047c7d17ae6a8073e6e5fdae000d07b176677
zhili <jiangzhili-1@thunisoft.com>
2017-03-16 20:21:19 +0800

    info :     1 use `try with resources` rewrite TimeClient
    author : chile

--- src/chile/netty/bio/TimeClient.java
+++ src/chile/netty/bio/TimeClient.java
@@ -25,42 +25,18 @@ public class TimeClient {
             }
         }
 
-        Socket socket = null;
-        BufferedReader in = null;
-        PrintWriter out = null;
-
-        try {
-            socket = new Socket("127.0.0.1", port);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
+        try(
+                Socket socket = new Socket("127.0.0.1", port);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+        ) {
             out.println("QUERY TIME ORDER");
             System.out.println("Send order to time server succeed !");
             String resp = in.readLine();
             System.out.println("Now is " + resp);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (out != null) {
-                out.close();
-                out = null;
-            }
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                socket = null;
-            }
         }
-
     }
 
 
