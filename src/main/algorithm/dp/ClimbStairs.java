6f2734f9be6db6ba7b5d0c8f5685b5443d739764
zhili <jiangzhili-1@thunisoft.com>
2017-06-19 18:53:26 +0800

    info :     1 调整目录
    author : chile

---
+++ src/main/algorithm/dp/ClimbStairs.java
@@ -0,0 +1,45 @@
+package main.algorithm.dp;
+
+import java.util.HashMap;
+
+/**
+ * Title : main.algorithm.dp
+ * Company : 北京华宇元典信息服务有限公司
+ * Description :
+ *  - 
+ *  -  
+ *
+ * @author chile
+ * @version 1.0
+ * @date 2017/6/19 16:45
+ */
+public class ClimbStairs {
+
+    public static void main(String[] args) {
+        int numOfStairs = 10;
+        System.out.println(getWaysToClimbStairs(numOfStairs));
+    }
+
+    private static int getWaysToClimbStairs(int n) {
+        HashMap<Integer, Integer> cache = new HashMap<>();
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        } else {
+            int temp = getWaysToClimbStairs(n - 1) + getWaysToClimbStairs(n - 2);
+            cache.put(n, temp);
+            return temp;
+        }
+    }
+
+
+
+}
