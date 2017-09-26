1554f5b2cd2e996502bc93b519914a33e24a61bd
zhili <jiangzhili-1@thunisoft.com>
2017-09-26 19:25:34 +0800

    add java-web/password

---
+++ java-web/password/password-web/src/main/java/work/zhili/password/PasswordWebApplication.java
@@ -0,0 +1,12 @@
+package work.zhili.password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordWebApplication.class, args);
+	}
+}
---
+++ java-web/password/password-web/src/test/java/work/zhili/password/PasswordWebApplicationTests.java
@@ -0,0 +1,16 @@
+package work.zhili.password;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PasswordWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
