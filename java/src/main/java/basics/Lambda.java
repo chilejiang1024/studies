577e95ae09214b5a5abafeee3f0ce90c6839ed73
Chile Jiang <chilejiang1024@gmail.com>
2017-12-23 22:00:56 +0800

    java basics : lambda

---
+++ java/src/main/java/basics/Lambda.java
@@ -0,0 +1,19 @@
+package java.basics;
+
+/**
+ * Title : java.basics
+ * Description :
+ * <ul>
+ * <li>
+ * </ul>
+ *
+ * @author chile
+ * @version 1.0
+ * @date 23/12/2017
+ */
+
+public class Lambda {
+    private static void function1() {
+
+    }
+}
