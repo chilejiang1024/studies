eac1f6da634eb4a4d9a3734ac503d6ced4ba56c9
chile <chilejiang1024@gmail.com>
2018-01-12 13:52:07 +0800

    Java util functional interface

---
+++ java/src/main/java/basics/JavaUtilFunctionalInterface.java
@@ -0,0 +1,27 @@
+package main.java.basics;
+
+import java.util.function.Function;
+
+/**
+ * Title : main.java.basics <br />
+ * Description :
+ *  <p>
+ *  <ul> 
+ *    <li>  </li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/1/12 13:38
+ */
+
+public class JavaUtilFunctionalInterface {
+
+    public static void main(String[] args) {
+        Function<String, Integer> appendStringFunction = (String a) -> {
+            System.out.println(a);
+            return 0;
+        };
+        appendStringFunction.apply("sha bi.");
+    }
+
+}
