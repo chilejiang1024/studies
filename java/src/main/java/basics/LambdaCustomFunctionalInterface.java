2ca6687627d6abd8f0ade9ea7632a879b81a51af
chile <chilejiang1024@gmail.com>
2018-01-12 13:37:09 +0800

    Lambda Custom Functional Interface

---
+++ java/src/main/java/basics/LambdaCustomFunctionalInterface.java
@@ -0,0 +1,38 @@
+package main.java.basics;
+
+/**
+ * Title : java.basics
+ * Description :
+ * <p> lambda basics
+ * <ul>
+ * <li> LambdaCustomFunctionalInterface
+ * </ul>
+ *
+ * @author chile
+ * @version 1.0
+ * @date 23/12/2017
+ */
+
+public class LambdaCustomFunctionalInterface {
+
+    interface MyLambdaFunctionalInterface {
+        /**
+         * perform the function
+         */
+        void perform();
+    }
+
+    private static void function1() {
+        MyLambdaFunctionalInterface greeter = () -> System.out.println("hello");
+        run(greeter);
+    }
+    
+    private static void run(MyLambdaFunctionalInterface lambda) {
+        System.out.println(lambda);
+        lambda.perform();
+    }
+    
+    public static void main(String[] args) {
+        function1();
+    }
+}
