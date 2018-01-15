164e28173d1b711d5ae4cc966cb7d83e551079ad
chile <chilejiang1024@gmail.com>
2018-01-15 17:38:56 +0800

    comparator

---
+++ java/src/main/java/basics/lambda/comparator/Human.java
@@ -0,0 +1,50 @@
+package main.java.basics.lambda.comparator;
+
+/**
+ * Title : main.java.basics.lambda.comparator
+ * Description :
+ *  <p>
+ *  <ul> 
+ *    <li>  </li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/1/15 10:10
+ */
+
+public class Human {
+
+    private String name;
+
+    private int age;
+
+    /**
+     * setters and getters
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age  = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %d.", this.name, this.age);
+    }
+}
