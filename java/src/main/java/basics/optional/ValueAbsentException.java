576901d30b0edfc8b9cb4bc4e6aeca7065b3eaa9
chile <chilejiang1024@gmail.com>
2018-01-18 15:00:28 +0800

    Optional usage in Java8

---
+++ java/src/main/java/basics/optional/ValueAbsentException.java
@@ -0,0 +1,18 @@
+package main.java.basics.optional;
+
+/**
+ * Title : main.java.basics.optional </br>
+ * Description : </br>
+ *  <p>
+ *  <ul> 
+ *    <li>  </li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/1/18 14:53
+ */
+public class ValueAbsentException extends Exception {
+    ValueAbsentException() {
+        System.out.println("Value absent.");
+    }
+}
