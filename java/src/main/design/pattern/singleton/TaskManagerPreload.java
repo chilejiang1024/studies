410c9c5d9318380f8f34844dd8ab9d4e95182f52
chile <chilejiang1024@gmail.com>
2018-01-22 18:31:26 +0800

    Preload singleton

---
+++ java/src/main/design/pattern/singleton/TaskManagerPreload.java
@@ -0,0 +1,15 @@
+package main.design.pattern.singleton;
+
+/**
+ * Title : main.design.pattern.singleton <br>
+ * Description :
+ *  <p> singleton instance
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/1/22 18:28
+ */
+public class TaskManagerPreload {
+}
