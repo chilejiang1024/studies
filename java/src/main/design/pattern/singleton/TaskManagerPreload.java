14720c922760c80c2b9932c79ac2352d38bd5606
chile <chilejiang1024@gmail.com>
2018-01-23 09:54:07 +0800

    Preload singleton

--- java/src/main/design/pattern/singleton/TaskManagerPreload.java
+++ java/src/main/design/pattern/singleton/TaskManagerPreload.java
@@ -3,13 +3,21 @@ package main.design.pattern.singleton;
 /**
  * Title : main.design.pattern.singleton <br>
  * Description :
- *  <p> singleton instance
+ *  <p> singleton INSTANCE
  *  <ul> 
- *      <li>
+ *      <li> 恶汉式单例, 天生线程安全.
  *  </ul>
  * @author chile
  * @version 1.0
  * @date 2018/1/22 18:28
  */
 public class TaskManagerPreload {
+
+    private static final TaskManagerPreload INSTANCE = new TaskManagerPreload();
+
+    private TaskManagerPreload() {}
+
+    public static TaskManagerPreload getInstance() {
+        return INSTANCE;
+    }
 }
