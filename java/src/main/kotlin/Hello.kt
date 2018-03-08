965efc4d309386a00791d61a771fa834137ac4d4
chile <chilejiang1024@gmail.com>
2018-03-08 17:43:40 +0800

    Hello kotlin

---
+++ java/src/main/kotlin/Hello.kt
@@ -0,0 +1,22 @@
+package main.kotlin
+
+/**
+ * Title : main <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description :
+ *  <p>
+ *  <ul>
+ *      <li>
+ *  </ul>
+ * @author  chile
+ * @version 1.0
+ * @date    2018/2/26 15:42
+ */
+class Hello {
+    var name: String? = "aa"
+}
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin.")
+    System.out.println("aaa")
+}
