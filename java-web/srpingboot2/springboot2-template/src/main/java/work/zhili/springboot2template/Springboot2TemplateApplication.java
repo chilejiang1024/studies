a4d0f9b0b68d52c76a4ca47822d77d611ddada59
zhili <chilejiang1024@gmail.com>
2018-03-21 17:28:59 +0800

    init spring boot 2

---
+++ java-web/srpingboot2/springboot2-template/src/main/java/work/zhili/springboot2template/Springboot2TemplateApplication.java
@@ -0,0 +1,12 @@
+package work.zhili.springboot2template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2TemplateApplication.class, args);
+	}
+}
