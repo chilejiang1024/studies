a892029981564316a4adce14a80da8d032736558
zhili <chilejiang1024@gmail.com>
2018-03-21 19:12:00 +0800

    handler exception in one place

---
+++ java-web/srpingboot2/springboot2-template/src/main/java/work/zhili/springboot2template/handler/ExceptionHandler.java
@@ -0,0 +1,62 @@
+package work.zhili.springboot2template.handler;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Title : work.zhili.springboot2template.handler <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/3/21 18:19
+ */
+@Component
+public class ExceptionHandler implements HandlerExceptionResolver {
+
+    /**
+     * the logger, get from slf4j
+     */
+    private static Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                         Object o, Exception e) {
+
+        logger.error("ExceptionHandler caught exception : ", e);
+
+        String requestType = httpServletRequest.getHeader("X-Requested-With");
+
+        // JSON格式返回
+        Map<String, Object> responseMap = new HashMap<>(4);
+        responseMap.put("code", -1);
+        responseMap.put("msg", "System is not available, please try it later.");
+        responseMap.put("result", new HashMap<>(2));
+        String json = new Gson().toJson(responseMap);
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json; charset=utf-8");
+        try {
+            httpServletResponse.getWriter().write(json);
+            httpServletResponse.getWriter().flush();
+            return null;
+        } catch (IOException ee) {
+            logger.error("", ee);
+        }
+
+        return new ModelAndView("redirect:/500.html");
+    }
+}
---
+++ java-web/srpingboot2/springboot2-template/src/main/java/work/zhili/springboot2template/service/FirstServiceImpl.java
@@ -0,0 +1,23 @@
+package work.zhili.springboot2template.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Title : work.zhili.springboot2template.service <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/3/21 18:42
+ */
+@Service
+public class FirstServiceImpl {
+
+    public void firstMethod() {
+        throw new RuntimeException();
+    }
+
+}
