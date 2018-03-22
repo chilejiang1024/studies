f901df11e14ca89179824255e6e61cf2eebd556d
zhili <chilejiang1024@gmail.com>
2018-03-22 10:02:21 +0800

    some changes

--- java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/controller/FirstController.java
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/controller/FirstController.java
@@ -3,7 +3,7 @@ package work.zhili.springboot2template.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import work.zhili.springboot2template.service.FirstServiceImpl;
+import work.zhili.springboot2template.service.impl.FirstServiceImpl;
 
 /**
  * Title : work.zhili.springboot2template.controller <br>
--- java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/service/IFirstService.java
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/service/IFirstService.java
@@ -1,9 +1,8 @@
 package work.zhili.springboot2template.service;
 
-import org.springframework.stereotype.Service;
-
 /**
  * Title : work.zhili.springboot2template.service <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
  * Description :
  *  <p>
  *  <ul> 
@@ -11,13 +10,10 @@ import org.springframework.stereotype.Service;
  *  </ul>
  * @author chile
  * @version 1.0
- * @date 2018/3/21 18:42
+ * @date 2018/3/22 10:01
  */
-@Service
-public class FirstServiceImpl {
+public interface IFirstService {
 
-    public void firstMethod() {
-        throw new RuntimeException();
-    }
+    void firstMethod();
 
 }
