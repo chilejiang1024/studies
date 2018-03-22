f84e9da8c7c20404aa1a2739ed5acdc9e1194180
zhili <chilejiang1024@gmail.com>
2018-03-22 09:58:09 +0800

    add javadoc

--- java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/Springboot2TemplateApplication.java
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/Springboot2TemplateApplication.java
@@ -3,6 +3,17 @@ package work.zhili.springboot2template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Title : work.zhili.springboot2template <br>
+ * Description :
+ *  <p>
+ *  <ul>
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/3/21 18:10
+ */
 @SpringBootApplication
 public class Springboot2TemplateApplication {
 
