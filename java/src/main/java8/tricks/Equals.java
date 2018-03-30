92bf3f2ab068a14a762ac9cb96f802055d52b2df
zhili <chilejiang1024@gmail.com>
2018-03-30 14:44:12 +0800

    add: `a == b || Objects.equals(a, b)` is recommended

--- java/src/main/java8/tricks/Equals.java
+++ java/src/main/java8/tricks/Equals.java
@@ -18,14 +18,28 @@ import static java.lang.System.out;
 public class Equals {
 
     public static void main(String[] args) {
-        Object a = "aaa";
-        Object b = 123;
+        Object a  = "aaa";
+        Object aa = "aaa";
+        Object b  = 123;
+        Object c  = a;
+        Object d  = null;
 
-        out.println(Objects.equals(a, b));
+        out.println(way1(a, b));
+        out.println(way1(a, d));
+        out.println(way1(a, aa));
+
+        out.println(way2(a, b));
+        out.println(way2(a, c));
+        out.println(way2(a, d));
+        out.println(way2(a, aa));
     }
 
+    private static boolean way1(Object a, Object b) {
+        return Objects.equals(a, b);
+    }
 
-
-
+    private static boolean way2(Object a, Object b) {
+        return a == b || Objects.equals(a, b);
+    }
 
 }
