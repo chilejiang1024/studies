b05b70bbe89c0fffc289ca102e91977dabc84388
zhili <chilejiang1024@gmail.com>
2018-04-08 13:39:52 +0800

    add: some ways to get class path

---
+++ java/src/main/java8/basics/concurrent/FutureObject.java
@@ -0,0 +1,25 @@
+package main.java8.basics.concurrent;
+
+/**
+ * Title : main.java8.basics.concurrent <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/3/30 13:40
+ */
+public class FutureObject {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+}
