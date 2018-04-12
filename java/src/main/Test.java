bd5a009c45292bfc7c41f549617d6ca9c0a6b892
zhili <chilejiang1024@gmail.com>
2018-04-12 10:21:21 +0800

    update: remove duplicate codes

--- java/src/main/Test.java
+++ java/src/main/Test.java
@@ -93,24 +93,6 @@ public class Test {
     }
 
     private static void test7() {
-        int[] a = {1, 2, 5, 7, 9};
-        int[] b = {1, 4, 6, 8, 9, 10, 17};
-        int i = 0, j = 0;
-        int n = 4;
-        while (n != 0) {
-            if (a[i] == b[j]) {
-                out.print(a[i]);
-                i++;
-                j++;
-            } else if (a[i] < b[j]) {
-                out.print(a[i]);
-                i++;
-            } else {
-                out.print(b[j]);
-                j++;
-            }
-            n--;
-        }
 
     }
 }
