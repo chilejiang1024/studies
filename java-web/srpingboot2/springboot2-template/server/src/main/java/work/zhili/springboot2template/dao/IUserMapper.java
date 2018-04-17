0fc32b63a3853f755613e5764ec19ae395bfde1b
zhili <chilejiang1024@gmail.com>
2018-04-17 13:22:14 +0800

    add: application-*.yml, mapper, model

---
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/dao/IUserMapper.java
@@ -0,0 +1,18 @@
+package work.zhili.springboot2template.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Title : work.zhili.springboot2template.dao <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/4/17 11:17
+ */
+@Component
+public interface IUserMapper {
+}
---
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/model/User.java
@@ -0,0 +1,15 @@
+package work.zhili.springboot2template.model;
+
+/**
+ * Title : work.zhili.springboot2template.model <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/4/17 11:18
+ */
+public class User {
+}
