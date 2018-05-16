9f9c3a0342545ccc53f8c39a0f5ca95cfdc7d546
zhili <chilejiang1024@gmail.com>
2018-05-16 10:13:11 +0800

    add: logger

--- java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/Springboot2TemplateApplication.java
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/Springboot2TemplateApplication.java
@@ -1,5 +1,7 @@
 package work.zhili.springboot2template;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,7 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot2TemplateApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(Springboot2TemplateApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Springboot2TemplateApplication.class, args);
+        logger.info("application was started ...");
     }
 }
