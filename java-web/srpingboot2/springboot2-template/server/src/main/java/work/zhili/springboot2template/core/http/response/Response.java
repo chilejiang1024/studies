86c75778a90a0c8d71f9ae2bfdf0235cd75e6657
zhili <chilejiang1024@gmail.com>
2018-05-24 16:33:18 +0800

    add: http response result

---
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/core/http/response/Response.java
@@ -0,0 +1,52 @@
+package work.zhili.springboot2template.core.http.response;
+
+/**
+ * Title : work.zhili.springboot2template.core.status <br>
+ * Description : <br>
+ *   <p> return message when the api request successfully
+ *
+ * @author chile
+ * @version 1.0
+ * @date 2018/5/22 13:13
+ */
+public class Response<E> {
+
+    /**
+     * default successful message
+     */
+    public static final Response SUCCESSFUL_MESSAGE;
+
+    static {
+        SUCCESSFUL_MESSAGE = successfulMessage();
+    }
+
+    private static Response successfulMessage() {
+        return new Response(ResultCode.SUCCESS, "Operation successful.");
+    }
+
+    private Integer code;
+
+    private String msg;
+
+    private String detail;
+
+    private E data;
+
+    public Response(ResultCode code) {
+        this.code = code.code();
+        this.msg  = code.message();
+    }
+
+    public Response(ResultCode code, String detail) {
+        this.code = code.code();
+        this.msg  = code.message();
+        this.detail = detail;
+    }
+
+    public Response(ResultCode code, E data) {
+        this.code = code.code();
+        this.msg  = code.message();
+        this.data = data;
+    }
+
+}
