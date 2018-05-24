b7bab09ac8a42427fb762d9996fd3bf2addecec5
zhili <chilejiang1024@gmail.com>
2018-05-24 14:53:48 +0800

    add: gson converters

---
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/configurer/HttpConfiguration.java
@@ -0,0 +1,32 @@
+package work.zhili.springboot2template.configurer;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Title : work.zhili.springboot2template.configurer <br>
+ * Description :
+ *  <p>
+ *  <ul> 
+ *      <li>
+ *  </ul>
+ * @author chile
+ * @version 1.0
+ * @date 2018/5/24 14:53
+ */
+@Configuration
+public class HttpConfiguration {
+    @Bean
+    public HttpMessageConverters customConverters() {
+        Collection<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+        messageConverters.add(gsonHttpMessageConverter);
+        return new HttpMessageConverters(true, messageConverters);
+    }
+}
