216a69f41c5db6cc2e5c4d5dc5c1f72917df21f8
chile <chilejiang1024@gmail.com>
2018-05-26 22:39:57 +0800

    add: some comments

--- java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/controller/UserController.java
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/controller/UserController.java
@@ -30,11 +30,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * query all, just for testing
+     * @return {user, user, ...}
+     */
     @GetMapping
     public Object queryAll() {
         return userService.queryAllForTesting();
     }
 
+    /**
+     * add a new user
+     * @param username user name
+     * @param password password
+     * @return ok
+     */
     @PostMapping
     public Object add(@RequestParam String username, @RequestParam String password) {
         User user = new User();
@@ -45,18 +55,33 @@ public class UserController {
         return Response.SUCCESSFUL_MESSAGE;
     }
 
+    /**
+     * invalid a user
+     * @param userId the user's id
+     * @return ok
+     */
     @DeleteMapping
     public Object delete(String userId) {
         userService.invalid(userId);
         return Response.SUCCESSFUL_MESSAGE;
     }
 
+    /**
+     * update a user
+     * @param user user
+     * @return ok
+     */
     @PatchMapping
     public Object update(@RequestBody User user) {
         userService.update(user);
         return Response.SUCCESSFUL_MESSAGE;
     }
 
+    /**
+     * get a user's details
+     * @param userId the user's id
+     * @return the user
+     */
     @GetMapping("/{userId}")
     public ResponseEntity<User> select(@PathVariable String userId) {
         User user = userService.select(userId);
