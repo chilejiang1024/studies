c46d2846eacb017d481289fe9243bdd99bb4913d
zhili <chilejiang1024@gmail.com>
2018-05-29 18:13:15 +0800

    add: file upload examples

---
+++ java-web/srpingboot2/springboot2-template/server/src/main/java/work/zhili/springboot2template/controller/FileUploadController.java
@@ -0,0 +1,29 @@
+package work.zhili.springboot2template.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Title : work.zhili.springboot2template.controller <br>
+ * Description : <br>
+ *   file upload examples
+ * @author chile
+ * @version 1.0
+ * @date 2018/5/29 18:00
+ */
+@RestController
+@RequestMapping("/api/v1/files")
+public class FileUploadController {
+
+    @PostMapping
+    public Object uploadFile(MultipartFile file) {
+
+        System.out.println(file.getOriginalFilename());
+
+        return ResponseEntity.ok("OK");
+    }
+
+}
