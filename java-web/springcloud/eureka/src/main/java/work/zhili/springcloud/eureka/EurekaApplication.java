7f694c832b536ecab2ab40d779bee0b18b7a904f
zhili <chilejiang1024@gmail.com>
2018-08-13 10:45:09 +0800

    feature: @EnableEurekaServer

--- java-web/springcloud/eureka/src/main/java/work/zhili/springcloud/eureka/EurekaApplication.java
+++ java-web/springcloud/eureka/src/main/java/work/zhili/springcloud/eureka/EurekaApplication.java
@@ -2,8 +2,20 @@ package work.zhili.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Title : work.zhili.springcloud.eureka.EurekaApplication <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description : <br>
+ *   eureka test
+ *
+ * @author zhili
+ * @version 1.0
+ * @date 2018-8-13 10:43:32
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
