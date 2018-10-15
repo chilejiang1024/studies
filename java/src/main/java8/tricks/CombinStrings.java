410b6181fdfb1ac191a2659b0b48c363c7936dd7
zhili <chilejiang1024@gmail.com>
2018-10-15 11:01:04 +0800

    trick: StringJoiner

---
+++ java/src/main/java8/tricks/CombinStrings.java
@@ -0,0 +1,25 @@
+package main.java8.tricks;
+
+import java.util.StringJoiner;
+
+/**
+ * Title : main.java8.tricks <br>
+ * Description :
+ *
+ *
+ * @author chile
+ * @version 1.0
+ * @date 2018/10/15 11:00
+ */
+public class CombinStrings {
+
+    public static void main(String[] args) {
+        StringJoiner joiner = new StringJoiner(",");
+        joiner.add("zhang1");
+        joiner.add("zhang2");
+        joiner.add("zhang3");
+        joiner.add("zhang4");
+        System.out.println(joiner.toString());
+    }
+
+}
