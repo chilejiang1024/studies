20d47fe8ea6158a5fa27e09cbf3e034d1803e7e6
zhili <chilejiang1024@gmail.com>
2019-04-29 14:46:18 +0800

    leetcode: Search Insert Position

---
+++ java/src/main/algorithm/leetcode/SearchInsertPosition.java
@@ -0,0 +1,14 @@
+package main.algorithm.leetcode;
+
+/**
+ * Title : main.algorithm.leetcode <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description :
+ *
+ *
+ * @author chile
+ * @version 1.0
+ * @date 2019/4/29 14:46
+ */
+public class SearchInsertPosition {
+}
