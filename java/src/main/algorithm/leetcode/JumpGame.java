b6048e1fa9d29bf9c84dd9091abe9bb5fa87d86e
zhili <chilejiang1024@gmail.com>
2019-06-19 15:00:19 +0800

    leetcode: jump game

---
+++ java/src/main/algorithm/leetcode/JumpGame.java
@@ -0,0 +1,36 @@
+package main.algorithm.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Title : main.algorithm.leetcode <br>
+ * Company : 北京华宇元典信息服务有限公司 <br>
+ * Description :
+ *   <p> Runtime: 1 ms, faster than 99.80% of Java online submissions for Jump Game.
+ *   <p> Memory Usage: 39.2 MB, less than 91.81% of Java online submissions for Jump Game.
+ * @author chile
+ * @version 1.0
+ * @date 2019/6/19 14:31
+ */
+public class JumpGame {
+
+    class Solution {
+        public boolean canJump(int[] nums) {
+            int n = nums.length, reach = 0;
+            for (int i = 0; i < n; i++) {
+                if (reach >= n - 1 || reach < i) {
+                    break;
+                }
+                reach = Math.max(reach, i + nums[i]);
+            }
+            return reach >= n - 1;
+        }
+    }
+
+    @Test
+    public void test1() {
+        int[] nums = { 2, 3, 1, 1, 4 };
+        assert new Solution().canJump(nums);
+    }
+
+}
