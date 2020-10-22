a5d4258f48c1f1ade0200218722f83c8af8cef8f
zhili <chilejiang1024@gmail.com>
2020-10-22 11:18:44 +0800

    java: 不同字符串声明方式的内存地址是否相等

--- java/src/main/Test.java
+++ java/src/main/Test.java
@@ -1,5 +1,9 @@
 package main;
 
+import org.apache.commons.io.Charsets;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
@@ -13,8 +17,8 @@ import static java.lang.System.out;
 /**
  * Title : PACKAGE_NAME
  * Description :
- *  - 
- *  -  
+ *  -
+ *  -
  *
  * @author main.chile
  * @version 1.0
@@ -150,7 +154,8 @@ public class Test {
         System.out.println(Integer.toBinaryString(-1));
     }
 
-    private static void test12() throws Exception {
+    @org.junit.Test
+    public void test12() throws Exception {
         System.out.println(Class.forName("[[[[[[[J").getName());
     }
 
@@ -161,7 +166,20 @@ public class Test {
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Authorization", "Basic bmF0YXMxNTpBd1dqMHc1Y3Z4clppT05nWjlKNXN0TlZrbXhkazM5Sg==");
         conn.setDoOutput(true);
+        conn.connect();
 
+        InputStream connIS = conn.getInputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        for (int b = connIS.read(); b > 0; b = connIS.read()) {
+            baos.write(b);
+        }
+
+        out.println(conn.getContentType());
+        out.println(conn.getContentEncoding());
+        out.println(new String(baos.toByteArray()));
+
+        connIS.close();
+        baos.close();
     }
 
     @org.junit.Test
@@ -210,19 +228,71 @@ public class Test {
         list.forEach(System.out::println);
     }
 
+    @org.junit.Test
+    public void test18() {
+        String goodsStr = "a,b,c,d";
+        List<String> goods_list1 = new ArrayList<>(Arrays.asList(goodsStr.trim().split(",")));
+        List<String> goods_list2 = Arrays.asList(goodsStr.trim().split(","));
+        out.println(goods_list1);
+        out.println(goods_list2);
+    }
+
+    @org.junit.Test
+    public void test19() {
+        int num = 0;
+        for (int i = 0; i < 100; i++) {
+            num = (num++);
+            out.println(num);
+        }
+        out.println(num);
+    }
+
+    @org.junit.Test
+    public void test20() {
+        int minutes = 139;
+        float hours = 0.00f;
+        hours = (float) minutes / 60.00f;
+        out.println(hours);
+        // hours += 0.5f;
+        out.println(String.format("%.2f", hours));
+    }
+
+    @org.junit.Test
+    public void test21() {
+        Map<String, String> map = new HashMap<>();
+        map.put("测试", "测试");
+        String test1 = "测试";
+        String test2 = new String("测试");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            out.println(entry.getKey() == entry.getValue());
+            out.println(test1 == entry.getKey());
+        }
+        out.println(test1 == test2);
+    }
+
+    private void add1() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("a");
+    }
+
+        private void add2() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("a");
+    }
+
     /**
      * the main method
      */
     public static void main(String[] args) throws Exception {
 
-        System.out.println(int.class);
+        out.println(String.format("%10s", "1"));
+        out.println(String.format("%-10s", "1"));
+        out.println(String.format("批量导出了%d个%s", 23, "123"));
 
+        for (byte b : "啊哈".getBytes(Charsets.UTF_8)) {
+            out.print(b);
+        }
     }
-
-
-
-
-
 
 
 
